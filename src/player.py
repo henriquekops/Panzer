fix player rotation

src/player.py
@@ -4,7 +4,8 @@
 # built-in dependencies
 from math import (
     sin,
-    cos
+    cos,
+    radians
 )
 
 # project dependencies
@@ -26,11 +27,11 @@ class Player:
     """
 
     def __init__(self, x:float, z:float) -> None:
-        self.alpha_camera= 45.0
+        self.alpha_camera = 5
         self.alpha_cannon = 0.0
         self.step_size = 1
         self.obs = np.array((x, 0.5, z))
-        self.tgt = np.array((x, 0.5, z+10.0))
+        self.tgt = np.array((x, 0.5, z+1.0))
         self.cam = np.array((0.0, 1.0, 0.0))
 
     def set_position(self, aspect_ratio):
@@ -59,7 +60,7 @@ class Player:
         glTranslated(self.obs[0], -0.5, self.obs[2]+ 1)
         glRotatef(self.alpha_cannon,0,1,0)
         glutSolidCube(1)
-        gluCylinder(quadric, 0.2, 0.2, 4, 5, 5);
+        gluCylinder(quadric, 0.2, 0.2, 4, 5, 5)
 
         glPopMatrix()
 
@@ -69,9 +70,9 @@ class Player:
         """
 
         if factor:
-            self.alpha_cannon -= 0.0
+            self.alpha_cannon -= self.alpha_camera
         else:
-            self.alpha_cannon += 0.0
+            self.alpha_cannon += self.alpha_camera
 
 
     def forward(self) -> None:
@@ -95,9 +96,9 @@ class Player:
         Look left 
         """
         self.tgt -= self.obs
-        self.tgt[0] = (self.tgt[0] * cos(self.alpha_camera)) + (self.tgt[2] * sin(self.alpha_camera))
+        self.tgt[0] = (self.tgt[0] * cos(radians(self.alpha_camera))) + (self.tgt[2] * sin(radians(self.alpha_camera)))
         self.tgt[1] = self.tgt[1]
-        self.tgt[2] = (-self.tgt[0] * sin(self.alpha_camera)) + (self.tgt[2] * cos(self.alpha_camera))
+        self.tgt[2] = (-self.tgt[0] * sin(radians(self.alpha_camera))) + (self.tgt[2] * cos(radians(self.alpha_camera)))
         self.tgt += self.obs
         print(self.tgt)
 
@@ -106,9 +107,9 @@ class Player:
         Look right
         """
         self.tgt -= self.obs
-        self.tgt[0] = (self.tgt[0] * cos(-self.alpha_camera)) + (self.tgt[2] * sin(-self.alpha_camera))
+        self.tgt[0] = (self.tgt[0] * cos(radians(-self.alpha_camera))) + (self.tgt[2] * sin(radians(-self.alpha_camera)))
         self.tgt[1] = self.tgt[1]
-        self.tgt[2] = (-self.tgt[0] * sin(-self.alpha_camera)) + (self.tgt[2] * cos(-self.alpha_camera))
+        self.tgt[2] = (-self.tgt[0] * sin(radians(-self.alpha_camera))) + (self.tgt[2] * cos(radians(-self.alpha_camera)))
         self.tgt += self.obs
         print(self.tgt)
 